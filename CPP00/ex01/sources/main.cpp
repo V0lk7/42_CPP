587b14dea3cde1167b91e65f1d98cef59cac2cb1
jean <jduval@3E6.42angouleme.fr>
2023-07-31 13:34:51 +0200

    modif user_pather in main.cpp ex00 cpp00

--- CPP00/ex01/sources/main.cpp
+++ CPP00/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 09:47:47 by jduval            #+#    #+#             */
-/*   Updated: 2023/07/25 09:48:07 by jduval           ###   ########.fr       */
+/*   Updated: 2023/07/31 13:34:06 by jduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int	main(void)
 	{
 		if (input.empty() == true)
 			std::cout << bad_input[0] << std::endl;
-		else if (input.compare("ADD") == 0)
+		else if (input.compare(user_pathern[0]) == 0)
 			(void)add_input(book);
-		else if (input.compare("SEARCH") == 0)
+		else if (input.compare(user_pathern[1]) == 0)
 			(void)search_input(book);
-		else if (input.compare("EXIT") == 0)
+		else if (input.compare(user_pathern[2]) == 0)
 			break ;
 		else
 			std::cout << bad_input[2] << std::endl;
